cd4a3433023923637a6e11025de404f921acd496
Adam Plewczyński <a332plewczynski@gmail.com>
2021-11-19 19:33:14 +0100

    Improved formatting

--- data_exploration.py
+++ data_exploration.py
@@ -3,46 +3,51 @@ from numpy import NaN
 import pandas
 
 
-datapath = './data/BFI_raw/bfi-weekend-box-office-report-2020-07-03-05.xls'
-df = pandas.read_excel( datapath, skiprows = 1 )
+datapath = "./data/BFI_raw/bfi-weekend-box-office-report-2020-07-03-05.xls"
+df = pandas.read_excel(datapath, skiprows=1)
+
+df.head(1000)
 
 print(df.columns)
 print(df.dtypes)
 print(df.shape)
-print (df.head(10))
-print (df.tail(5))
+
+print(df.head(10))
+print(df.tail(5))
 
 #######  After concatenating sheets   ########
 ##############################################
 
-datapath = './data/BFI_Merged.xlsx'
-df = pandas.read_excel( datapath )
+datapath = "./data/BFI_Merged.xlsx"
+df = pandas.read_excel(datapath)
 
-#initial stats
+# initial stats
 print(df.describe(datetime_is_numeric=True))
-print('Rows x Columns:  ' + str(df.shape))
+print("Rows x Columns:  " + str(df.shape))
 print(df.dtypes)
 
-df.boxplot(column=['Weekend Gross'])
-plt.yscale('log') #very big discrepancy, thus log scale
+df.boxplot(column=["Weekend Gross"])
+plt.yscale("log")  # very big discrepancy, thus log scale
 plt.show()
-df.boxplot(column=['Weeks on release'])
+df.boxplot(column=["Weeks on release"])
 plt.show()
-df.boxplot(column=['API Budget'])
+df.boxplot(column=["API Budget"])
 plt.show()
 
-#Plot premiere week revenue over time (very big discrepancy, thus log scale)
-g1 = df.loc[ df['Weeks on release'] == 1 ].plot.scatter( x = 'API Release Date', y = 'Weekend Gross', s=0.5)
-g1.set_yscale('log')
-plt.savefig( './data/graphs/g1.png' )
+# Plot premiere week revenue over time (very big discrepancy, thus log scale)
+g1 = df.loc[df["Weeks on release"] == 1].plot.scatter(
+    x="API Release Date", y="Weekend Gross", s=0.5
+)
+g1.set_yscale("log")
+plt.savefig("./data/graphs/g1.png")
 plt.show()
 
 # Plot rating against revenue
-df.loc[ df['API Budget'] != NaN ].plot.scatter( x = 'API Budget', y = 'API Rating', s=0.5)
-plt.savefig( fname='./data/graphs/g2.png' )
+df.loc[df["API Budget"] != NaN].plot.scatter(x="API Budget", y="API Rating", s=0.5)
+plt.savefig(fname="./data/graphs/g2.png")
 plt.show()
 
 # Plot Budget against revenue
-df.loc[ df['API Budget'] != NaN ].plot.scatter( x = 'API Budget', y = 'Weekend Gross', s=0.5)
-plt.savefig( fname='./data/graphs/g3.png' )
+df.loc[df["API Budget"] != NaN].plot.scatter(x="API Budget", y="Weekend Gross", s=0.5)
+plt.savefig(fname="./data/graphs/g3.png")
 plt.show()
--- data_preparation.py
+++ data_preparation.py
@@ -5,65 +5,76 @@ import glob
 import datetime
 
 # get all the xls files in the folder
-datapath = './data/BFI_raw/'
-columns = ['Rank', 'Film', 'Weekend Gross', 'Weeks on release', 'Number of cinemas', 'Site average','Total Gross to date'] #columns to read
+datapath = "./data/BFI_raw/"
+columns = [
+    "Rank",
+    "Film",
+    "Weekend Gross",
+    "Weeks on release",
+    "Number of cinemas",
+    "Site average",
+    "Total Gross to date",
+]  # columns to read
 
-xls_files = glob.glob( os.path.join( datapath, "*.xls") )
+xls_files = glob.glob(os.path.join(datapath, "*.xls"))
 
-#create an empty dataframe that will be output
+# create an empty dataframe that will be output
 d_main = pandas.DataFrame()
 
 for a in xls_files:
-    d_temp = pandas.read_excel( a, usecols=columns, skiprows = 1 )
+    d_temp = pandas.read_excel(a, usecols=columns, skiprows=1)
 
-    #drop lines starting with an empty cell (these are not film data)
-    d_temp.dropna(subset = ['Rank','Film'], inplace = True)
+    # drop lines starting with an empty cell (these are not film data)
+    d_temp.dropna(subset=["Rank", "Film"], inplace=True)
 
-    #merge it with the main one
-    d_main = pandas.concat( [d_main, d_temp], ignore_index=True )
+    # merge it with the main one
+    d_main = pandas.concat([d_main, d_temp], ignore_index=True)
 
-#drop the rank column, was needed only to recognise actual film info
-d_main.drop( 'Rank', axis = 1, inplace = True )
+# drop the rank column, was needed only to recognise actual film info
+d_main.drop("Rank", axis=1, inplace=True)
 
 
 ####   tmdb API part    #####
 #############################
 import tmdbsimple as tmdb
-tmdb.API_KEY = 'd9a2c308dbb2a7268990206ecf3028cf'
+
+tmdb.API_KEY = "d9a2c308dbb2a7268990206ecf3028cf"
 d_temp = pandas.DataFrame()
-d_temp['API Genres'] = None
+d_temp["API Genres"] = None
 search = tmdb.Search()
 
 for ind, row in d_main.iterrows():
 
-    print( "Quering for entry " + str(ind + 1) + " out of " + str(len(d_main.index)) )
+    print("Quering for entry " + str(ind + 1) + " out of " + str(len(d_main.index)))
 
-    search.movie( language='en', query = row['Film'] )
+    search.movie(language="en", query=row["Film"])
 
-    #add genre, length, release date and rating
+    # add genre, length, release date and rating
     if search.results:
-        movie_object = tmdb.Movies( search.results[0]['id'] )
+        movie_object = tmdb.Movies(search.results[0]["id"])
         movie_info = movie_object.info()
-        d_main.at[ind, 'API Title'] = movie_info['title']
-        d_main.at[ind, 'API length'] = movie_info['runtime']        
-        if movie_info['vote_count'] > 5: d_main.at[ind, 'API Rating'] = movie_info['vote_average'] 
-        if movie_info['budget'] != 0: d_main.at[ind, 'API Budget'] = movie_info['budget']
-        d_main.at[ind, 'API Release Date'] = movie_info['release_date']
+        d_main.at[ind, "API Title"] = movie_info["title"]
+        d_main.at[ind, "API length"] = movie_info["runtime"]
+        if movie_info["vote_count"] > 5:
+            d_main.at[ind, "API Rating"] = movie_info["vote_average"]
+        if movie_info["budget"] != 0:
+            d_main.at[ind, "API Budget"] = movie_info["budget"]
+        d_main.at[ind, "API Release Date"] = movie_info["release_date"]
         genres = []
-        for i in movie_info['genres']:
-                genres.append( i['name'] )
-        d_temp.loc[ind, 'API Genres'] = genres
+        for i in movie_info["genres"]:
+            genres.append(i["name"])
+        d_temp.loc[ind, "API Genres"] = genres
 
-# 
-d_main = pandas.concat( [d_main, d_temp], axis=1, join='inner')
+#
+d_main = pandas.concat([d_main, d_temp], axis=1, join="inner")
 
-#drop films not found by the API and ones released before 2016(either rescreenings or misassigned by the API)
-#print( d_main[ d_main['API Title'].isnull() ] )    # <== Proof
+# drop films not found by the API and ones released before 2016(either rescreenings or misassigned by the API)
+# print( d_main[ d_main['API Title'].isnull() ] )    # <== Proof
 
-d_main.dropna(subset = ['API Title'], inplace = True)
-d_main = d_main.loc[ d_main['API Title'] == d_main['Film'] ]
-d_main['API Release Date'] = pandas.to_datetime(d_main['API Release Date'])
-d_main = d_main.loc[ d_main['API Release Date'].dt.year >= 2016 ]
+d_main.dropna(subset=["API Title"], inplace=True)
+d_main = d_main.loc[d_main["API Title"] == d_main["Film"]]
+d_main["API Release Date"] = pandas.to_datetime(d_main["API Release Date"])
+d_main = d_main.loc[d_main["API Release Date"].dt.year >= 2016]
 
-#export one xlsx file (newer format)
-d_main.to_excel( excel_writer = 'data/BFI_merged.xlsx' )
+# export one xlsx file (newer format)
+d_main.to_excel(excel_writer="data/BFI_merged.xlsx")
